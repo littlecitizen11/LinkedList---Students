4c8e90a868742f96d1d8278f24999869dc2375f5
LittleCitizen11 <thelittlecitizen@gmail.com>
2020-08-11 13:29:16 +0300

    added the given sln, README and gitignore

---
+++ LinkedList - Students/LinkedList/CirclularLinkedListValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    public class CirclularLinkedListValidator 
+    {
+        public bool IsCircular<T>(LinkedList<T> linkedList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
