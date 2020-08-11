d51e5ba9e851683fba2cde25efe0460d6d746324
LittleCitizen11 <thelittlecitizen@gmail.com>
2020-08-11 13:55:46 +0300

    add function of isInfiniteLinkedList

--- LinkedList - Students/LinkedList/CirclularLinkedListValidator.cs
+++ LinkedList - Students/LinkedList/CirclularLinkedListValidator.cs
@@ -5,6 +5,19 @@ namespace LinkedList
 {
     public class CirclularLinkedListValidator 
     {
+        public bool isLinkedListInfinite<T>(LinkedList<T> linkedList)
+        {
+            LinkedListNode<T> position1 = linkedList.First;
+            LinkedListNode<T> position2 = (linkedList.First);
+            while (position1 != null && position2 != null && position2.Next != null)
+            {
+                position1 = position1.Next;
+                position2 = position2.Next.Next;
+                if (position1 == position2)
+                    return true;
+            }
+            return false;
+        }
         public bool IsCircular<T>(LinkedList<T> linkedList)
         {
             throw new NotImplementedException();
